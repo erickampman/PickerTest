3dc8f62646a40a144330f83272d76575cab94650
Eric Kampman <erickampman@yahoo.com>
2024-04-18 06:38:27 -0700

    Initial Commit

---
+++ PickerTest/PickerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerTestApp.swift
+//  PickerTest
+//
+//  Created by Eric Kampman on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PickerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
